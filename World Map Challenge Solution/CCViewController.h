39ad8c40009f9370a0ff74dd83f253ae76776844
Eliot Arntz <arntzel@gmail.com>
2013-10-19 15:04:25 +0200

    Initial Commit

---
+++ World Map Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  World Map Challenge Solution
+//
+//  Created by Eliot Arntz on 10/19/13.
+//  Copyright (c) 2013 Code Coalition. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
